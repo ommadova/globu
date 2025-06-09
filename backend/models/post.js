33e50f73d4025451759eabc48b19645a5441773d
ommadova <ommadovaa67@gmail.com>
2025-06-09 11:34:57 -0500

    Adding Schema

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,18 +1,41 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose");
 
-const postSchema = new Schema(
+const imageSchema = new mongoose.Schema(
   {
-    content: { type: String, required: true },
-    author: {
-      type: Schema.Types.ObjectId,
+    url: { type: String, required: true },
+  },
+  { _id: false }
+);
+
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
       required: true,
-      ref: 'User'
-    }
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const postSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    country: { type: String, required: true },
+    content: { type: String },
+    places: [String],
+    foods: [String],
+    drinks: [String],
+
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    favoritedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [commentSchema],
+
+    images: [imageSchema],
   },
   {
     timestamps: true,
   }
 );
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model("Post", postSchema);
