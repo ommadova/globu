7f1071d33660665a5a2c332e38219b93029c00dd
ommadova <ommadovaa67@gmail.com>
2025-06-11 14:33:56 -0500

    Add show function

--- frontend/src/services/postService.js
+++ frontend/src/services/postService.js
@@ -1,11 +1,15 @@
 import sendRequest from "./sendRequest";
 
-const BASE_URL = '/api/posts';
+const BASE_URL = "/api/posts";
 
 export async function index() {
   return sendRequest(BASE_URL);
 }
 
 export async function create(postData) {
-  return sendRequest(BASE_URL, 'POST', postData);
+  return sendRequest(BASE_URL, "POST", postData);
+}
+
+export async function show(postId) {
+  return sendRequest(`${BASE_URL}/${postId}`);
 }
