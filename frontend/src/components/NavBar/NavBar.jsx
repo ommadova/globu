619aeafbefb785224a6307f9b64564909afe4368
ommadova <ommadovaa67@gmail.com>
2025-06-17 14:52:49 -0500

    favicon

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -8,40 +8,40 @@ export default function NavBar({ user, setUser }) {
   function handleLogOut() {
     logOut();
     setUser(null);
-    // The <Link> that was clicked will navigate to "/"
+    navigate("/");
   }
 
   return (
     <nav className="NavBar">
-      <Link to="/">
-        <img src="/images/logo.svg" alt="plane icon" className="logo" />
-      </Link>
-      <NavLink to="/">Home</NavLink>
-      &nbsp; | &nbsp;
-      <NavLink to="/posts" end>
-        Explore
-      </NavLink>
-      &nbsp; | &nbsp;
-      {user ? (
-        <>
-          <NavLink to="/myposts">My Posts</NavLink>
-          &nbsp; | &nbsp; &nbsp; &nbsp;
-          <NavLink to="/posts/new">New Post</NavLink>
-          &nbsp; | &nbsp;
-          <Link to="/" onClick={handleLogOut}>
-            Log Out
-          </Link>
-          <span>
-            Welcome, {user.name.charAt(0).toUpperCase() + user.name.slice(1)}
-          </span>
-        </>
-      ) : (
-        <>
-          <NavLink to="/login">Log In</NavLink>
-          &nbsp; | &nbsp;
-          <NavLink to="/signup">Sign Up</NavLink>
-        </>
-      )}
+      <img src="/images/logo.svg" alt="globe icon" className="logo" />
+      <div className="nav-links">
+        <NavLink to="/">Home</NavLink>
+        &nbsp; | &nbsp;
+        <NavLink to="/posts" end>
+          Explore
+        </NavLink>
+        &nbsp; | &nbsp;
+        {user ? (
+          <>
+            <NavLink to="/myposts">My Posts</NavLink>
+            &nbsp; | &nbsp; &nbsp; &nbsp;
+            <NavLink to="/posts/new">New Post</NavLink>
+            &nbsp; | &nbsp;
+            <Link to="/" onClick={handleLogOut}>
+              Log Out
+            </Link>
+            <span>
+              Welcome, {user.name.charAt(0).toUpperCase() + user.name.slice(1)}
+            </span>
+          </>
+        ) : (
+          <>
+            <NavLink to="/login">Log In</NavLink>
+            &nbsp; | &nbsp;
+            <NavLink to="/signup">Sign Up</NavLink>
+          </>
+        )}
+      </div>
     </nav>
   );
 }
